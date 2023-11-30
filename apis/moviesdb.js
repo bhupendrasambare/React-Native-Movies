d41969e0e2d630d6423402ff93f09c36e1073496
Bhupendra sambare <bhupendrasam1404@gmail.com>
2023-12-01 02:58:31 +0530

    Minor changes

--- apis/moviesdb.js
+++ apis/moviesdb.js
@@ -50,6 +50,7 @@ export const fetchMovieSimilar = id =>{
     return apiCall(movieSimilarEndpint(id));
 }
 
+
 export const image500 = path => path?`https://image.tmdb.org/t/p/w500${path}`:null;
 export const image342 = path => path?`https://image.tmdb.org/t/p/w342${path}`:null;
 export const image185 = path => path?`https://image.tmdb.org/t/p/w185${path}`:null;
